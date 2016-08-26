4e49b0441ff67584a48e21d429f0e0383d7f0f22
admin <love-nankai@163.com>
2016-08-26 12:38:42 +0800

    ~

---
+++ iOSBaseProject/DetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailViewController.swift
+//  iOSBaseProject
+//
+//  Created by admin on 16/8/26.
+//  Copyright © 2016年 Ding. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func backButtonTapped() {
+        navigationController?.popToRootViewControllerAnimated(true)
+    }
+
+}
