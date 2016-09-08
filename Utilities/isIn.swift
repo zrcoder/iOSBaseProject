a862fe330b47f89ac2b37e989bec20fba985ee4c
admin <love-nankai@163.com>
2016-09-08 09:24:53 +0800

    ~

---
+++ Utilities/isIn.swift
@@ -0,0 +1,20 @@
+//
+//  isIn.swift
+//  iOSBaseProject
+//
+//  Created by admin on 16/9/8.
+//  Copyright © 2016年 Ding. All rights reserved.
+//
+
+import Foundation
+
+public extension Equatable {
+    public func isIn(collection: Self...) -> Bool {
+        return collection.contains(self)
+    }
+    /*
+    public func isIn<T: SequenceType where T.Generator.Element: Equatable>(collection: T) -> Bool {
+        return collection.contains(self)
+    }
+    */
+}
