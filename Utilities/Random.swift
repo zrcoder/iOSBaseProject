3581f2ace1b1fb287068a058f9318fe600a9ed50
admin <love-nankai@163.com>
2016-11-02 11:39:49 +0800

    ~

--- Utilities/Random.swift
+++ Utilities/Random.swift
@@ -19,14 +19,6 @@ public extension Int {
     public static func random(_ lower: Int = 0, _ upper: Int = 100) -> Int {
         return lower + Int(arc4random_uniform(UInt32(upper - lower + 1)))
     }
-    
-    /**
-     - returns: range.startIndex <= value <= range.endIndex
-     */
-    public static func random(_ range: Range<Int>) -> Int {
-        return Int.random(range.lowerBound, range.upperBound - 1)
-    }
-    
     /// if self == 0, 0; else if self > 0, 0 <= value <= self; else self <= value <= 0
     public var random: Int {
         return Int.random(0, self)
@@ -43,15 +35,23 @@ public extension UInt32 {
         assert(upper >= lower, "UInt32.random(lower:upper:) - upper should >= lower")
         return lower + arc4random_uniform(upper - lower + 1)
     }
-    /**
-     - returns: range.startIndex <= value <= range.endIndex
-     */
-    public static func random(_ range: Range<UInt32>) -> UInt32 {
-        return UInt32.random(range.lowerBound, range.upperBound - 1)
-    }
     /// 0 <= value <= self
     public var random: UInt32 {
         return UInt32.random(0, self)
+    }
+}
+
+public extension CountableClosedRange where Bound: Integer {
+    /// lower <= value <= upper
+    public var random: Int {
+        return lowerBound.hashValue + arc4random_uniform(UInt32(upperBound.hashValue - lowerBound.hashValue)).hashValue
+    }
+}
+
+public extension CountableRange where Bound: Integer {
+    /// lower <= value < upper
+    public var random: Int {
+        return lowerBound.hashValue + arc4random_uniform(UInt32(upperBound.hashValue - lowerBound.hashValue)).hashValue
     }
 }
 
