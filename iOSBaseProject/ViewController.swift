1bedca920559e180790121748f37a91e5c9bcf73
admin <love-nankai@163.com>
2016-11-02 11:44:40 +0800

    ~

--- iOSBaseProject/ViewController.swift
+++ iOSBaseProject/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         dLog("Hello, world!")
-        Int.random(1...3)
     }
 
     override func didReceiveMemoryWarning() {
